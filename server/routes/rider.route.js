c438b6b5853b237481ea91d043fc83755be9ca81
Chester Lynn <chester.htooaunglinn@gmail.com>
2021-04-29 02:10:50 +0630

    fix: reformat codebase

--- server/routes/rider.route.js
+++ server/routes/rider.route.js
@@ -1,10 +1,10 @@
-const riderController = require("../controllers/rider.controller");
-const router = require("express").Router();
-const multer = require("multer");
+const router = require('express').Router();
+const multer = require('multer');
+const riderController = require('../controllers/rider.controller');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/spring_delivery");
+    cb(null, 'public/spring_delivery');
   },
   filename: (req, file, cb) => {
     if (!file) {
@@ -19,14 +19,14 @@ const uploadStore = multer({
   storage,
 });
 
-router.post("/api/rider", riderController.registerNewRider);
-router.get("/api/riders", riderController.getAllRiders);
-router.get("/api/rider/:id", riderController.getOneRider);
-router.delete("/api/rider/:id", riderController.deleteRider);
-router.put("/api/rider/:id", riderController.editRider);
+router.post('/api/rider', riderController.registerNewRider);
+router.get('/api/riders', riderController.getAllRiders);
+router.get('/api/rider/:id', riderController.getOneRider);
+router.delete('/api/rider/:id', riderController.deleteRider);
+router.put('/api/rider/:id', riderController.editRider);
 router.post(
-  "/api/rider/upload",
-  uploadStore.single("file"),
-  riderController.uploadRiderImage
+  '/api/rider/upload',
+  uploadStore.single('file'),
+  riderController.uploadRiderImage,
 );
 module.exports = router;
